fca2b6d664b4afdf38640fbd5ed5aa964afb3b81
Fribb <daraku@live.de>
2019-04-10 19:39:20 +0200

    Add Plugins support #8
    Added Plugin Support for SubZero

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -20,7 +20,7 @@ AGENT_NAME = "MyAnimeList.net Agent"
 AGENT_VERSION = "v6.0.0"
 AGENT_LANGUAGES = [Locale.Language.English]
 AGENT_PRIMARY_PROVIDER = True
-AGENT_ACCEPTS_FROM = [ 'com.plexapp.agents.localmedia', 'com.plexapp.agents.opensubtitles' ]
+AGENT_ACCEPTS_FROM = [ 'com.plexapp.agents.localmedia', 'com.plexapp.agents.opensubtitles', 'com.plexapp.agents.subzero' ]
 AGENT_CACHE_TIME = CACHE_1HOUR * 24
 
 AGENT_MAPPING_URL = "https://atarashii.fribbtastic.net/mapping/animeMapping_full.json"
